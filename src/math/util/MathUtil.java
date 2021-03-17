205c6bed9554d037c5404b64e07f3275f22a4e67
Gia Bao <giabaotran912@gmail.com>
2021-03-17 15:46:58 +0700

    up

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,17 @@
+package math.util;
+
+/**
+ *
+ * @author 9550
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("This is the first message");
+    }
+
+}
