1439cf9969d829f35697e270059ee49fc9be441e
Gia Bao <giabaotran912@gmail.com>
2021-03-26 15:07:53 +0700

    Update core to add the recursion

--- src/com/giabao/mathutil/MathUtility.java
+++ src/com/giabao/mathutil/MathUtility.java
@@ -11,18 +11,27 @@ public class MathUtility {
 
     public static final double PI = 3.1415;
 
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. n must be between 0..20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long res = 1;
+//        for (int i = 2; i <= n; i++) {
+//            res = res * i;
+//        }
+//        return res;
+//    }
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
+        if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid argument. n must be between 0..20");
-        }
-        if (n == 0 || n == 1) {
+        
+        if (n == 0 || n == 1) 
             return 1;
-        }
-        long res = 1;
-        for (int i = 2; i <= n; i++) {
-            res = res * i;
-        }
-        return res;
+        
+        return n * getFactorial(n - 1);
     }
-
+    
 }
